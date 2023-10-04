4eca630557ef4a656331418690abac6a9baa0da8
QobiljonIbaydullayevDev <qobiljonyoqubovich2002@gmail.com>
2023-10-04 20:50:42 +0500

    Worked on styles

--- nCounter/script.js
+++ nCounter/script.js
@@ -1,16 +1,19 @@
 const add = document.body.querySelector('.btn-add ');
 const sub = document.body.querySelector('.btn-sub');
+const clear =document.body.querySelector('.btn-clear');
 const res = document.body.querySelector('.result')
 
 let count = 0
 const addFun = () => {
     count ++
-    console.log(count);
     res.innerHTML=count
 }
 
 const subFun = () => {
     count --
-    console.log(count);
     res.innerHTML = count
+}
+
+const clearFun =()=>{
+    res.innerHTML=count-count
 }
